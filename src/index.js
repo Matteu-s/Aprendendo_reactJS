b38e26ea9c533e8f6f8387e893eb3df965fb7275
Mateus <mateeussantoos@hotmail.com>
2020-06-22 21:34:18 -0300

    basico react js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./styles.css"
 
+function  sum(a, b){
+    return a + b
+}
+
+function primeiroJSX(){
+    return(
+        <div className="teste">
+            Mateus Santos
+    <h1>Soma: {sum(10, 20)}</h1>
+        </div>
+    )
+}
+
 const App = () => {
 
     return (
         <div className="App">
-            Hello World
+            {primeiroJSX()}
+            {}
         </div>
     )
 }
